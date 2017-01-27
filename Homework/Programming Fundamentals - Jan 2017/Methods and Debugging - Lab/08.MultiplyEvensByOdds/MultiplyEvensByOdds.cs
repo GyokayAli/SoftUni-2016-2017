ed3e08193a8e3ec5bb083a4dc01470fcc2d94235
Gyokay Ali <gyokay.metin.ali@gmail.com>
2017-01-27 22:56:25 +0200

    Updated lab task 8

--- Homework/Programming Fundamentals - Jan 2017/Methods and Debugging - Lab/08.MultiplyEvensByOdds/MultiplyEvensByOdds.cs
+++ Homework/Programming Fundamentals - Jan 2017/Methods and Debugging - Lab/08.MultiplyEvensByOdds/MultiplyEvensByOdds.cs
@@ -6,10 +6,44 @@
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
+            int n = Math.Abs(int.Parse(Console.ReadLine()));
 
-            //int sum = GetMultipleOfEvensByOdds(n);
-            //Console.WriteLine(sum);
+            int result = GetSumOfEvenDigits(n) * GetSumOfOddDigits(n);
+            Console.WriteLine(result);
+        }
+
+        static int GetSumOfEvenDigits(int n)
+        {
+            int sumEvens = 0;
+            while (n > 0)
+            {
+                int lastDigit = n % 10;
+                if (lastDigit % 2 == 0)
+                {
+                    sumEvens += lastDigit;
+                }
+
+                n /= 10;
+            }
+
+            return sumEvens;
+        }
+
+        static int GetSumOfOddDigits(int n)
+        {
+            int sumOdds = 0;
+            while (n > 0)
+            {
+                int lastDigit = n % 10;
+                if (lastDigit % 2 != 0)
+                {
+                    sumOdds += lastDigit;
+                }
+
+                n /= 10;
+            }
+
+            return sumOdds;
         }
     }
 }
