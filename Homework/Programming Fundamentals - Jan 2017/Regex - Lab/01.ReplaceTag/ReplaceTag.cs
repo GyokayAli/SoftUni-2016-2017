0872544c109ad4b3a9bb53ea35e5da0615f6d74a
Gyokay Ali <gyokay.metin.ali@gmail.com>
2017-02-18 19:29:22 +0200

    Added lab solutions

---
+++ Homework/Programming Fundamentals - Jan 2017/Regex - Lab/01.ReplaceTag/ReplaceTag.cs
@@ -0,0 +1,23 @@
+﻿namespace _01.ReplaceTag
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class ReplaceTag
+    {
+        public static void Main()
+        {
+            string html = Console.ReadLine();
+
+            while (html != "end")
+            {
+                string pattern = @"<a.*?href.*?=(.*)>(.*?)<\/a>";
+                string replace = @"[URL href=$1]$2[/URL]";
+                string replaced = Regex.Replace(html, pattern, replace);
+
+                Console.WriteLine(replaced);
+                html = Console.ReadLine();
+            }
+        }
+    }
+}
