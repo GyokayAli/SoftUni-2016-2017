7b5ac740715e85e022460b8d2abdc789f0356ccd
Gyokay Ali <gyokay.metin.ali@gmail.com>
2017-02-26 10:41:15 +0200

    Added Programming Fundamentals Exam (26.02.2017) Prep Solutions

--- Exam Prep/ProgFundExamPreparation3/03.RageQuit/RageQuit.cs
+++ Exam Prep/ProgFundExamPreparation3/03.RageQuit/RageQuit.cs
@@ -3,12 +3,34 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-
+    using System.Text;
+    using System.Text.RegularExpressions;
     public class RageQuit
     {
-        public void Main()
+        public static void Main()
         {
+            var input = Console.ReadLine().ToUpper();
+            var pattern = @"(\D+)(\d+)";
+            var regex = new Regex(pattern);
+            int uniqueSymbolsCount = 0;
 
+            var matches = regex.Matches(input);
+            var sb = new StringBuilder();
+
+            foreach (Match match in matches)
+            {
+                var repetitions = int.Parse(match.Groups[2].ToString());
+                var symbol = match.Groups[1].ToString();
+
+                for (int i = 0; i < repetitions; i++)
+                {
+                    sb.Append(symbol);
+                }
+            }
+
+            uniqueSymbolsCount = sb.ToString().Distinct().Count();
+            Console.WriteLine($"Unique symbols used: {uniqueSymbolsCount}");
+            Console.WriteLine(sb);
         }
     }
 }
---
+++ Exam Prep/ProgFundExamPreparation3/04.Files/Files.cs
@@ -0,0 +1,65 @@
+﻿namespace _04.Files
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Files
+    {
+        public static void Main()
+        {
+            var storageDict = new Dictionary<string, Dictionary<string, long>>();
+
+            var n = int.Parse(Console.ReadLine());
+            var counter = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(new char[] { '\\' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] fileData = input.Last().Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                var root = input[0];
+                var fileName = fileData[0];
+                var fileSize = long.Parse(fileData[1]);
+
+                if (storageDict.ContainsKey(root))
+                {
+                    if (storageDict[root].ContainsKey(fileName))
+                    {
+                        storageDict[root][fileName] = fileSize;
+                    }
+                    else
+                    {
+                        storageDict[root].Add(fileName, fileSize);
+                    }
+                }
+                else
+                {
+                    storageDict[root] =  new Dictionary<string, long> { { fileName, fileSize } };
+                }
+            }
+
+            string[] command = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var extension = command[0];
+            var dir = command[2];
+
+            var sorted = storageDict.Where(x => x.Key == dir).OrderByDescending(f => f.Value.Values);
+
+            foreach (var r in sorted)
+            {
+                foreach (var kvp in r.Value.OrderByDescending(x => x.Value).ThenBy(z => z.Key))
+                {
+                    if (kvp.Key.Split('.').Last() == extension)
+                    {
+                        Console.WriteLine($"{kvp.Key} - {kvp.Value} KB");
+                        counter++;
+                    }
+                }
+            }
+
+            if (counter == 0)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
